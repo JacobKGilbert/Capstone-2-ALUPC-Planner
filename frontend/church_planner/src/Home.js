36a8aacef847ffb0608ab9b2853a24c4a0a593a0
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-12-14 22:41:05 -0600

    refactored to utilize useAuth

--- frontend/church_planner/src/Home.js
+++ frontend/church_planner/src/Home.js
@@ -1,58 +1,41 @@
-import React, { useContext, useCallback, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button, ButtonGroup } from 'reactstrap'
-import ChurchPlannerApi from './api'
 import AuthContext from './AuthContext'
 
 const Home = () => {
-  const { currUser, setCurrUser } = useContext(AuthContext)
-
+  const { currUser, getUser } = useContext(AuthContext)
   const navigate = useNavigate()
 
-  const getUser = useCallback(async (id) => {
-      const user = await ChurchPlannerApi.getUser(id)
-      setCurrUser(user)
-    }, [setCurrUser])
-
   useEffect(() => {
-    const localToken = JSON.parse(localStorage.getItem('token'))
     const id = JSON.parse(localStorage.getItem('id'))
+
     if (!currUser) {
-      if (localToken && id) {
+      if (id) {
         getUser(id)
       }
     } else {
-      if (localToken && id) {
-        navigate(`/users/${id}`)
+      if (!id) {
+        navigate('/login')
       }
+
+      navigate(`/users/${id}`)
     }
   }, [currUser, getUser, navigate])
 
-  if (!currUser) {
-    return (
-      <div className="Home d-flex flex-column justify-content-center align-items-center">
-        <h1>Church Planner</h1>
-        <p>Get the right volunteers, in the right places.</p>
-        <ButtonGroup>
-          <Link to="/login">
-            <Button outline color="primary" size="md">
-              Log In
-            </Button>
-          </Link>
-        </ButtonGroup>
-      </div>
-    )
-  } else {
-    return (
-      <div className="Home d-flex flex-column justify-content-center align-items-center">
-        <h1>Church Planner</h1>
-        <p>Get the right volunteers, in the right places.</p>
-        <h3>
-          Welcome, {currUser.firstName} {currUser.lastName}!
-        </h3>
-      </div>
-    )
-  }
+  return (
+    <div className="Home d-flex flex-column justify-content-center align-items-center">
+      <h1>Church Planner</h1>
+      <p>Get the right volunteers, in the right places.</p>
+      <ButtonGroup>
+        <Link to="/login">
+          <Button outline color="primary" size="md">
+            Log In
+          </Button>
+        </Link>
+      </ButtonGroup>
+    </div>
+  )
 }
 
 export default Home
