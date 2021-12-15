5b8121413c96066ac6a2afe54a7af1b3c23fc321
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-12-14 23:49:19 -0600

    refactored useEffect, added if/else for rendering

--- frontend/church_planner/src/Home.js
+++ frontend/church_planner/src/Home.js
@@ -1,41 +1,37 @@
 import React, { useContext, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { Button, ButtonGroup } from 'reactstrap'
 import AuthContext from './AuthContext'
 
 const Home = () => {
   const { currUser, getUser } = useContext(AuthContext)
-  const navigate = useNavigate()
+  const id = JSON.parse(localStorage.getItem('id'))
 
   useEffect(() => {
-    const id = JSON.parse(localStorage.getItem('id'))
-
     if (!currUser) {
       if (id) {
         getUser(id)
       }
-    } else {
-      if (!id) {
-        navigate('/login')
-      }
+    } 
+  }, [currUser, getUser, id])
 
-      navigate(`/users/${id}`)
-    }
-  }, [currUser, getUser, navigate])
-
-  return (
-    <div className="Home d-flex flex-column justify-content-center align-items-center">
-      <h1>Church Planner</h1>
-      <p>Get the right volunteers, in the right places.</p>
-      <ButtonGroup>
-        <Link to="/login">
-          <Button outline color="primary" size="md">
-            Log In
-          </Button>
-        </Link>
-      </ButtonGroup>
-    </div>
-  )
+  if (!currUser) {
+    return (
+      <div className="Home d-flex flex-column justify-content-center align-items-center">
+        <h1>Church Planner</h1>
+        <p>Get the right volunteers, in the right places.</p>
+        <ButtonGroup>
+          <Link to="/login">
+            <Button outline color="primary" size="md">
+              Log In
+            </Button>
+          </Link>
+        </ButtonGroup>
+      </div>
+    )
+  } else {
+    return <Navigate to={`/users/${id}`} />
+  } 
 }
 
 export default Home
