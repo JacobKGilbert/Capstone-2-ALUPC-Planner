a329e97b75f32f72f9704a88eee1c9c4c7fc0a74
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-12-21 22:12:57 -0600

    added a check for if currUser is admin or is selected profile

--- frontend/church_planner/src/Profile.js
+++ frontend/church_planner/src/Profile.js
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, Navigate, useParams } from 'react-router-dom'
 import { Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap'
 import UnavailableForm from './UnavailableForm'
 import AuthContext from './AuthContext'
@@ -7,54 +7,47 @@ import EventCard from './EventCard'
 import MyTooltip from './Tooltip'
 
 const Profile = () => {
-  const { currUser, getUser } = useContext(AuthContext)
-  const id = JSON.parse(localStorage.getItem('id'))
-
-  useEffect(() => {
-    const gU = async (id) => await getUser(id)
-
-    if (!currUser) {
-      if (id) {
-        gU(id)
-      }
-    }
-  }, [currUser, getUser, id])
+  const { currUser } = useContext(AuthContext)
+  const { id } = useParams()
 
   if (currUser) {
-    return (
-      <div className="row">
-        <h1>
-          {currUser.firstName} {currUser.lastName}'s Profile
-        </h1>
-        <div className="col-xl-9 col-lg-8 col-md-7 col-sm-6 mt-2">
-          <h2>My Schedule</h2>
-          {currUser.events.map((ev) => (
-            <EventCard key={ev.id} event={ev} />
-          ))}
+    if (currUser.id === id || currUser.isAdmin) {
+      return (
+        <div className="row">
+          <h1>
+            {currUser.firstName} {currUser.lastName}'s Profile
+          </h1>
+          <div className="col-xl-9 col-lg-8 col-md-7 col-sm-6 mt-2">
+            <h2>My Schedule</h2>
+            {currUser.events.map((ev) => (
+              <EventCard key={ev.id} event={ev} />
+            ))}
+          </div>
+          <div className="col-xl-3 col-lg-4 col-md-5 col-sm-6 mt-4">
+            <h5>
+              Going to be out of town?{' '}
+              <span>
+                <MyTooltip />
+              </span>
+            </h5>
+            <UnavailableForm />
+            <Card>
+              <CardHeader tag="h3">Links</CardHeader>
+              <CardBody>
+                <CardTitle tag="h5">Special Title Treatment</CardTitle>
+                <CardText>
+                  <Link to="update">
+                    Update Profile
+                  </Link>
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
         </div>
-        <div className="col-xl-3 col-lg-4 col-md-5 col-sm-6 mt-4">
-          <h5>
-            Going to be out of town?{' '}
-            <span>
-              <MyTooltip />
-            </span>
-          </h5>
-          <UnavailableForm />
-          <Card>
-            <CardHeader tag="h3">Links</CardHeader>
-            <CardBody>
-              <CardTitle tag="h5">Special Title Treatment</CardTitle>
-              <CardText>
-                <Link to="update">
-                  Update Profile
-                </Link>
-              </CardText>
-            </CardBody>
-          </Card>
-          <Outlet />
-        </div>
-      </div>
-    )
+      )
+    } else {
+      <Navigate to={`users/${currUser.id}`} />
+    }
   } else {
     return <h1>Loading...</h1>
   }
