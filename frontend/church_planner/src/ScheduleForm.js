0d685e84bd00fe6380aca3c69e3ad54180907d13
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-12-23 00:01:57 -0600

    created

---
+++ frontend/church_planner/src/ScheduleForm.js
@@ -0,0 +1,10 @@
+
+const ScheduleForm = () => {
+  return (
+    <div className="col-md-4 position-absolute top-50 start-50 translate-middle">
+      <h3>Schedule</h3>
+    </div>
+  )
+}
+
+export default ScheduleForm
