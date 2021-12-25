e80edbf1d7a9d54726926a9bda1e9f06658ba359
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-12-25 15:31:29 -0600

    added type for rendering different results

--- frontend/church_planner/src/EventCard.js
+++ frontend/church_planner/src/EventCard.js
@@ -2,15 +2,26 @@ import React from 'react'
 // import { useNavigate } from 'react-router-dom'
 import { Card, CardTitle, CardText } from 'reactstrap'
 
-const EventCard = ({ event }) => {
+const EventCard = ({ event, type }) => {
   const date = event.date.split('T')[0]
 
-  return (
-    <Card body>
-      <CardTitle tag="h5">{date}</CardTitle>
-      <CardText>{event.deptName} - {event.positionName}</CardText>
-    </Card>
-  )
+  if (type === 'user') {
+    return (
+      <Card body>
+        <CardTitle tag="h5">{date}</CardTitle>
+        <CardText>
+          {event.deptName} - {event.positionName}
+        </CardText>
+      </Card>
+    )
+  }
+  if (type === 'department') {
+    return (
+      <Card body>
+        <CardTitle tag="h5">{date}</CardTitle>
+      </Card>
+    )
+  }
 }
 
 export default EventCard
