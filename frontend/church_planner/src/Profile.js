933deba484be6e3a7dbab2755f85f34ddcbcf3b1
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-12-25 14:42:03 -0600

    refactored if statement

--- frontend/church_planner/src/Profile.js
+++ frontend/church_planner/src/Profile.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
+import ChurchPlannerApi from './api'
 import UnavailableForm from './UnavailableForm'
 import AuthContext from './AuthContext'
 import EventCard from './EventCard'
@@ -7,19 +8,35 @@ import MyTooltip from './Tooltip'
 
 const Profile = () => {
   const { currUser } = useContext(AuthContext)
+  const [ user, setUser ] = useState()
   const { id } = useParams()
 
-  if (currUser) {
-    if (currUser.id === id || currUser.isAdmin) {
+  useEffect(() => {
+    const getUser = async (id) => {
+      const user = await ChurchPlannerApi.getUser(id)
+      setUser(user)
+    }
+
+    if (!user) {
+      getUser(id)
+    }
+  }, [user, id])
+
+  if (!user) {
+    return <h1>Loading...</h1>
+  } else {
+    if (!(currUser.id === id || currUser.isAdmin)) {
+      return <Navigate to={`users/${currUser.id}`} />
+    } else {
       return (
         <div className="row">
           <h1>
-            {currUser.firstName} {currUser.lastName}'s Profile
+            {user.firstName} {user.lastName}'s Profile
           </h1>
           <div className="col-xl-9 col-lg-8 col-md-7 col-sm-6 mt-2">
             <h2>My Schedule</h2>
-            {currUser.events.map((ev) => (
-              <EventCard key={ev.id} event={ev} />
+            {user.events.map((ev) => (
+              <EventCard key={ev.id} event={ev} type="user"/>
             ))}
           </div>
           <div className="col-xl-3 col-lg-4 col-md-5 col-sm-6 mt-4">
@@ -33,11 +50,7 @@ const Profile = () => {
           </div>
         </div>
       )
-    } else {
-      <Navigate to={`users/${currUser.id}`} />
     }
-  } else {
-    return <h1>Loading...</h1>
   }
 }
 
